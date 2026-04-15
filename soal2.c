b5b7ea1533838d981ccd16b2beeb269650b7122f
Adli Syauqi <121152721+adli-syauqi@users.noreply.github.com>
2026-04-15 08:33:52 +0700

    soal 2 praktikum 1 (15/04/2026)
    Implement recursive function to calculate steps for given integers.

--- soal2.c
+++ soal2.c
@@ -1,5 +1,43 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : Overview of C Language
+ *   Hari dan Tanggal    : 15 April 2026
+ *   Nama (NIM)          : Muhammad Adli Syauqi (13224082)
+ *   Nama File           : soal2.c
+ *   Deskripsi           : Program menerima n bilangan integer yang akan diiterasi terhadap suatu fungsi matematis untuk dihitung jumlah langkah di rekursifnya
+ * 
+ */
+
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+int rekursif(int n, int count){
+    if(n == 1){
+        return count;
+    }
+
+    count++;
+    
+    if(n%2 == 1){
+        n = n*3 + 1;
+    }else{
+        n = n/2;
+    }
+
+    return rekursif(n, count);
+}
+
+int main(){
+    int T;
+    scanf("%d", &T);
+
+    int array[T];
+
+    for(int i = 0; i < T; i++){
+        scanf("%d", &array[i]);
+    }
+
+    for(int i = 0; i < T; i++){
+        int count = 0;
+        array[i] = rekursif(array[i], count);
+        printf("LANGKAH %d\n\n", array[i]);
+    }
 }
